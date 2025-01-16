178f2245fd7db32c2f8ec63baecdb47bbf1a7451
James Valladolid <valladolid5823@gmail.com>
2025-01-16 11:07:25 -0600

    Base branch

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,45 +1,15 @@
 const routes = [
   {
     path: '/',
-    redirect: {
-      name: 'products'
-    },
     component: () => import('layouts/MainLayout.vue'),
     children: [
       {
-        path: 'products',
-        name: 'products',
-        component: () => import('pages/ProductList.vue')
+        path: '',
+        component: () => import('pages/IndexPage.vue')
       },
-      {
-        path: 'dashboard/vendor',
-        component: () => import('pages/SupplierDashboard.vue'),
-        meta: { requiresAuth: true, role: 'supplier' }
-      },
-      {
-        path: 'dashboard/buyer',
-        component: () => import('pages/RetailerDashboard.vue'),
-        meta: { requiresAuth: true, role: 'retailer' }
-      },
-      // {
-      //   path: 'login',
-      //   component: () => import('pages/Login.vue')
-      // },
-      // {
-      //   path: 'register',
-      //   component: () => import('pages/Register.vue')
-      // },
-      // {
-      //   path: 'profile',
-      //   component: () => import('pages/Profile.vue'),
-      //   meta: { requiresAuth: true }
-      // },
+
     ]
   },
-  // {
-  //     path: '/login',
-  //     component: () => import('pages/Login.vue')
-  //   },
 
   // Always leave this as last one,
   // but you can also remove it
