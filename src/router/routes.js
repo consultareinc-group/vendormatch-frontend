199bbca7b3d4da30a2b64235a0d922ab389eb5eb
James Valladolid <valladolid5823@gmail.com>
2025-01-23 10:27:07 -0600

    rename pages folder

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,12 +5,12 @@ const routes = [
     children: [
       {
         path: 'dashboard/vendor',
-        component: () => import('src/pages/Supplier/SupplierDashboard.vue'),
+        component: () => import('src/pages/supplier/SupplierDashboard.vue'),
         meta: { requiresAuth: true, role: 'supplier' }
       },
       {
         path: 'dashboard/buyer',
-        component: () => import('src/pages/Buyer/BuyerDashboard.vue'),
+        component: () => import('src/pages/buyer/BuyerDashboard.vue'),
         meta: { requiresAuth: true, role: 'retailer' }
       },
     ]
