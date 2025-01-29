34ff1211ef04d04ef1882d282494c04ceb26920e
James Valladolid <valladolid5823@gmail.com>
2025-01-29 13:41:24 -0600

    Create Login Page

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,27 +1,49 @@
-import { defineStore } from 'pinia';
-import { ref } from 'vue';
+import { defineStore } from 'pinia'; // Import Pinia's defineStore to create a state management store
+import { api } from 'boot/axios'; // Import the axios instance for API requests
+import { LocalStorage } from 'quasar';
 
-export const useAuthStore = defineStore('auth', () => {
-  const user = ref({
-    // role: 'buyer',
-    role: 'vendor',
-  });
-  const isAuthenticated = ref(false);
+// Define a Pinia store named 'counter' for managing users-related data
+export const useUserStore = defineStore('login', {
+  state: () => ({
+    UserInformation: {}
+  }),
+  actions: {
+    // Action to fetch specific users from the API
+    GetUser(request) {
+      return new Promise((resolve, reject) => {
+        // Make a GET request to fetch users based on the offset
+        api.get(`vendor-match/login/${request.id}`).then((response) => {
+          resolve(response.data); // Resolve the promise with the API response data
+        }).catch((response) => {
+          reject(response.data); // Reject the promise if the API request fails
+        });
+      });
+    },
 
-  function setUser(userData) {
-    user.value = userData;
-    isAuthenticated.value = true;
-  }
+    // Action to insert users
+    LoginUser(request) {
+      return new Promise((resolve, reject) => {
+        // Make a POST request to insert users in the database users table
+        api
+          .post(`vendor-match/login`, request)
+          .then((response) => {
+            if (response.data.data.status === 'success') {
+              SetBearerToken(response.data.data.message.bearer_toker)
+            }
+            resolve(response.data); // Resolve the promise with the API response data
+          })
+          .catch((error) => {
+            reject(error); // Reject the promise if the API request fails
+          });
+      });
+    },
+    SetBearerToken(token) {
+      if (token !== null) {
+        LocalStorage.set('Bearer', token);
+      } else {
+        LocalStorage.remove('Bearer');
+      }
+    };
 
-  function logout() {
-    user.value = null;
-    isAuthenticated.value = false;
-  }
-
-  return {
-    user,
-    isAuthenticated,
-    setUser,
-    logout
-  };
-});
+  },
+})
