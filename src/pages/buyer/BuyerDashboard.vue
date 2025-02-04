2277c1285e41f70d76d6589cbf325b05c2140778
James Valladolid <valladolid5823@gmail.com>
2025-02-04 11:22:46 -0600

    Use Product Card component

--- src/pages/buyer/BuyerDashboard.vue
+++ src/pages/buyer/BuyerDashboard.vue
@@ -70,65 +70,7 @@
         <q-card>
           <q-card-section>
             <div class="text-h6">Recommended Products</div>
-            <div class="row q-col-gutter-md q-mt-md">
-              <!-- Products Grid -->
-              <div v-if="!recommendedProducts.length" class="col-12">
-                <div class="row q-col-gutter-sm">
-                  <div v-for="n in 8" :key="n" class="col-12 col-sm-6 col-md-3">
-                    <q-card class="product-card">
-                      <q-skeleton height="192px" width="190px" class="full-width"></q-skeleton>
-
-                      <q-card-section class="q-pa-none q-pl-sm q-mt-sm q-pr-sm">
-                        <q-skeleton square></q-skeleton>
-                        <q-skeleton square></q-skeleton>
-                        <q-skeleton square></q-skeleton>
-                      </q-card-section>
-
-                      <q-card-actions align="right" class="q-pa-none q-mt-sm q-pb-sm q-pr-sm">
-                        <q-skeleton square width="158px" height="24px"></q-skeleton>
-                      </q-card-actions>
-                    </q-card>
-                  </div>
-                </div>
-              </div>
-              <div v-else class="col-12">
-                <div class="row q-col-gutter-sm">
-                  <div
-                    @click="showProductDetailsDialog(product)"
-                    v-for="product in recommendedProducts"
-                    :key="product.id"
-                    class="col-12 col-sm-6 col-md-3 cursor-pointer"
-                  >
-                    <q-card class="product-card">
-                      <q-img
-                        :src="`data:image/jpeg;base64,${product?.image[0]?.binary ?? ''}`"
-                        :ratio="1"
-                        alt="Product Image"
-                      />
-
-                      <q-card-section>
-                        <div class="text-weight-medium product-name">{{ product?.name }}</div>
-                        <div class="text-subtitle2">{{ product?.category }}</div>
-                        <div class="text-body2 q-mt-sm">
-                          <span class="text-grey">Cost:</span> ${{ product?.size[0]?.cost ?? 0 }}
-                        </div>
-                      </q-card-section>
-
-                      <q-card-actions class="flex justify-between items-center cursor-pointer">
-                        <q-icon name="favorite_border" color="secondary" size="sm" />
-                        <q-btn flat color="primary" no-caps label="Connect to Vendor" />
-                        <!-- <q-btn
-                  v-if="authStore.user?.role === 'retailer'"
-                  color="primary"
-                  label="Contact Supplier"
-                /> -->
-                      </q-card-actions>
-                    </q-card>
-                  </div>
-                </div>
-              </div>
-              <ViewProductDetails v-if="triggerStore.ViewProductDetailsDialog" />
-            </div>
+            <ProductCard class="q-mt-md" />
           </q-card-section>
         </q-card>
       </div>
@@ -139,15 +81,13 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import { useProductStore } from 'src/stores/products'
-import { useTriggerStore } from 'src/stores/triggers'
 // import { useAuthStore } from 'src/stores/auth'
 // Import Quasar framework utilities
 import { useQuasar } from 'quasar'
-import ViewProductDetails from '../../components/ViewProductDetails.vue'
+import ProductCard from '../../components/ProductCard.vue'
 import SavedProducts from './components/SavedProducts.vue'
 
 const productStore = useProductStore()
-const triggerStore = useTriggerStore()
 // Initialize Quasar for UI utilities
 const $q = useQuasar()
 
@@ -206,11 +146,6 @@ onMounted(() => {
       })
     })
 })
-
-const showProductDetailsDialog = (product_details) => {
-  triggerStore.ViewProductDetailsDialog = true
-  productStore.ProductDetails = product_details
-}
 </script>
 
 <style lang="scss" scoped>
